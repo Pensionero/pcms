6e093948bb059aad84a5f552481690f5780a949e
Pensionero <Pems.67@yandex.ru>
2020-06-23 23:44:55 +0300

    Начало5

---
+++ engine/Cms.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Engine;
+
+
+class Cms 
+{
+   /**
+    * @var DI
+   */
+   private $di;
+
+
+   /**
+   * Cms constructor.
+   * @param $di
+   */
+
+   public function __construct($di)
+   {
+      $this->di = $di;
+   }
+
+
+   /**
+   * Run Cms
+   */
+   public function run()
+   {
+      print_r($this->di);
+   }
+
+}
